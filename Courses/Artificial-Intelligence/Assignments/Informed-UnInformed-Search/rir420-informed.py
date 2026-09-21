8f5aa60c82f02e11b98fc618ce5f10d7d1149e00
BTCMauxri <mauriciov4795@gmail.com>
2026-09-21 14:25:36 -0500

    Defined: get_points method

--- Courses/Artificial-Intelligence/Assignments/Informed-UnInformed-Search/rir420-informed.py
+++ Courses/Artificial-Intelligence/Assignments/Informed-UnInformed-Search/rir420-informed.py
@@ -10,5 +10,35 @@ to goal state (bottom right to top left).
 import heapq
 
 # Function to read 'maze.txt'
-def read_maze(file):
+def read_maze(path):
+
+    # open and read file, adds lines to a 2D array
+    with open(path, 'r') as file:
+        return [list(line.rstrip('\n')) for line in file.readlines()]
+
+# function takes inthe  2D array (maze)
+def get_points(maze):
+
+    # Get the start and goal states of the maze
+    rows = len(maze)
+    columns = len(maze[0])
+
+    # goal/exit = 1st space in top row of array
+    goal = (0, maze[0].index(' '))
+
+    # start last space in last row
+    start = (rows - 1, "".join(maze[rows - 1]).rindex(' '))
+
+    return start, goal
+
+
+
+def main():
+
+    # clarify maze.txt file path
+    maze = read_maze("maze.txt")
+
     
+
+if __name__ == "__main__":
+        main()
