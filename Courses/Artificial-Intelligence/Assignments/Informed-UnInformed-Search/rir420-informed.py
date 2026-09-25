0a8339f601ab7f895cdc9ab1139260529f4f0a65
BTCMauxri <mauriciov4795@gmail.com>
2026-09-25 12:34:06 -0500

    Finished and tested script

--- Courses/Artificial-Intelligence/Assignments/Informed-UnInformed-Search/rir420-informed.py
+++ Courses/Artificial-Intelligence/Assignments/Informed-UnInformed-Search/rir420-informed.py
@@ -46,17 +46,78 @@ def get_adjacent_states(position, maze, rows, columns):
 
     for d_row, d_column in moves:
          new_row, new_column = row + d_row, column + d_column
-         if 0 <= new_row < rows and 0 <= new_column < columns and maze[new_row][new_column] != '0':
+         if 0 <= new_row < rows and 0 <= new_column < columns and maze[new_row][new_column] != 'O':
               adj_states.append((new_row, new_column))
 
     return adj_states
 
+# Implementing the A* search Algorithm
+def a_star_search(maze):
+
+    start, goal = get_points(maze)
+    rows, columns = len(maze), len(maze[0])
+
+    # priority queue
+    frontier = []
+    heapq.heappush(frontier, (0, start))
+
+    prior_state = {start: None}
+    current_cost = {start: 0}
+
+    while frontier:
+        # rid lowest score
+        current_priority, current_node = heapq.heappop(frontier)
+
+        if current_node == goal:
+               break
+
+        # go through each valid adj states that are free spaces
+        for state in get_adjacent_states(current_node, maze, rows, columns):
+            new_cost = current_cost[current_node] + 1
+
+            if state not in current_cost or new_cost < current_cost[state]:
+                current_cost[state] = new_cost
+                priority = new_cost + get_manhattan_distance(state, goal)
+                heapq.heappush(frontier, (priority, state))
+                prior_state[state] = current_node
+
+    # no path to goal scenario
+    if goal not in prior_state:
+         return None, 0
+
+    # Re-trace the optimal path to goal
+    path = []
+    current_state = goal
+    while current_state != start:
+         path.append(current_state)
+         current_state = prior_state[current_state]
+    path.append(start)
+
+    return path, current_cost[goal]
+ 
 def main():
-
-    # clarify maze.txt file path
+    # Read maze array
     maze = read_maze("maze.txt")
-
     
+    # Begin search
+    path, final_cost = a_star_search(maze)
+    
+    # Output
+    if path:
+        # Make Optimal Path
+        print("\nPath for maze.txt:\n")
+        for row, col in path:
+            maze[row][col] = '*'
+            
+        # Maze grid
+        for row in maze:
+            print("".join(row))
+            
+        # Path cost
+
+        print(f"\nCost of optimal path: {final_cost}\n")
+    else:
+        print("Solution not found")
 
 if __name__ == "__main__":
-        main()
+    main()
